86e4885e23e66200a8d3da1c1ed935add40c6200
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2026-07-01 12:22:09 -0500

    fix: client url for prod

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,7 +4,6 @@ import { createAuthClient } from "better-auth/react";
 import { app } from "@/lib/config/app.config";
 
 export const authClient = createAuthClient({
-  baseURL: app.url,
   plugins: [stripeClient()],
 });
 
